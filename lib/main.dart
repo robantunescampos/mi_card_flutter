1c13afc3d579be03cc87c925bfc5e05fdeada973
Robson Antunes <86518792+robantunescampos@users.noreply.github.com>
2024-01-31 19:59:00 -0300

    Aplicativo do módulo do bootcamp de flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,70 @@ void main() {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 backgroundImage: AssetImage('images/robson.jpg'),
+                radius: 50.0,
               ),
               Text(
                 "Robson Antunes",
                 style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                    fontFamily: 'Source Sans',
+                    letterSpacing: 2.5,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 150.0,
+                child: Divider(color: Colors.white70,),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+55 15 99625-1205",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans',
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "robantunescampos@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans',
+                      fontSize: 18.0,
+                    ),
+                  ),
                 ),
               )
             ],
